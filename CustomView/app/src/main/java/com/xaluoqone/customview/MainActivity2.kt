bc49fcbc61fddbd8ed7f1c6f36d29d06e6e9b1b4
xaluoqone <1617321643@qq.com>
2021-03-27 16:53:21 +0800

    add TestView

--- CustomView/app/src/main/java/com/xaluoqone/customview/MainActivity2.kt
+++ CustomView/app/src/main/java/com/xaluoqone/customview/MainActivity2.kt
@@ -2,14 +2,19 @@ package com.xaluoqone.customview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.SeekBar
 import androidx.appcompat.widget.AppCompatSeekBar
 import com.xaluoqone.customview.ex.dp
 import com.xaluoqone.customview.widget.RoundAvatarView
+import com.xaluoqone.customview.widget.TestView
 
 class MainActivity2 : AppCompatActivity() {
     private lateinit var avatar: RoundAvatarView
     private lateinit var avatarBorderWidthPlus: AppCompatSeekBar
+    private lateinit var testView: TestView
+    private lateinit var nextMode: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +22,8 @@ class MainActivity2 : AppCompatActivity() {
 
         avatar = findViewById(R.id.avatar)
         avatarBorderWidthPlus = findViewById(R.id.avatarBorderWidthPlus)
+        testView = findViewById(R.id.testView)
+        nextMode = findViewById(R.id.nextMode)
 
         avatarBorderWidthPlus.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
@@ -31,5 +38,10 @@ class MainActivity2 : AppCompatActivity() {
 
             }
         })
+
+        nextMode.setOnClickListener {
+            testView.porterDuffModeNumber++
+            Log.e("当前模式", "${testView.porterDuffModeNumber}")
+        }
     }
 }
---
+++ CustomView/app/src/main/java/com/xaluoqone/customview/widget/TestView.kt
@@ -0,0 +1,74 @@
+package com.xaluoqone.customview.widget
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import com.xaluoqone.customview.ex.dp
+
+/**
+ * 本View只用于测试xfermode的17个PorterDuff.Mode
+ */
+class TestView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val circleBitmap =
+        Bitmap.createBitmap(180.dp.toInt(), 180.dp.toInt(), Bitmap.Config.ARGB_8888)
+    private val rectBitmap =
+        Bitmap.createBitmap(180.dp.toInt(), 180.dp.toInt(), Bitmap.Config.ARGB_8888)
+    private var xfermode: PorterDuffXfermode
+    var porterDuffModeNumber = 0
+        set(value) {
+            field = if (value > 17) 0 else value
+            xfermode = PorterDuffXfermode(getPorterDuffModeNumber(porterDuffModeNumber))
+            requestLayout()
+            invalidate()
+        }
+
+    init {
+        xfermode = PorterDuffXfermode(getPorterDuffModeNumber(porterDuffModeNumber))
+        val circleCanvas = Canvas(circleBitmap)
+        paint.color = Color.RED
+        circleCanvas.drawCircle(120.dp, 60.dp, 60.dp, paint)
+        val rectCanvas = Canvas(rectBitmap)
+        paint.color = Color.BLUE
+        rectCanvas.drawRect(0f, 60.dp, 120.dp, 180.dp, paint)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        val saveCount = canvas.saveLayer(
+            width / 4f,
+            height / 2f - 90.dp,
+            width / 2f + 90.dp,
+            height / 2f + 90.dp,
+            paint
+        )
+        canvas.drawBitmap(circleBitmap, width / 4f, height / 2f - 90.dp, paint)
+        paint.xfermode = xfermode
+        canvas.drawBitmap(rectBitmap, width / 4f, height / 2f - 90.dp, paint)
+        paint.xfermode = null
+        canvas.restoreToCount(saveCount)
+    }
+
+    private fun getPorterDuffModeNumber(porterDuffModeNumber: Int) =
+        when (porterDuffModeNumber) {
+            0 -> PorterDuff.Mode.CLEAR
+            1 -> PorterDuff.Mode.SRC
+            2 -> PorterDuff.Mode.DST
+            3 -> PorterDuff.Mode.SRC_OVER
+            4 -> PorterDuff.Mode.DST_OVER
+            5 -> PorterDuff.Mode.SRC_IN
+            6 -> PorterDuff.Mode.DST_IN
+            7 -> PorterDuff.Mode.SRC_OUT
+            8 -> PorterDuff.Mode.DST_OUT
+            9 -> PorterDuff.Mode.SRC_ATOP
+            10 -> PorterDuff.Mode.DST_ATOP
+            11 -> PorterDuff.Mode.XOR
+            12 -> PorterDuff.Mode.DARKEN
+            13 -> PorterDuff.Mode.LIGHTEN
+            14 -> PorterDuff.Mode.MULTIPLY
+            15 -> PorterDuff.Mode.SCREEN
+            16 -> PorterDuff.Mode.ADD
+            17 -> PorterDuff.Mode.OVERLAY
+            else -> throw IllegalAccessException("参数不正确：$porterDuffModeNumber")
+        }
+}
