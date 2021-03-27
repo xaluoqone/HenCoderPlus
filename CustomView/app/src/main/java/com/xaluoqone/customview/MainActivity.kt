637d6f9c51cda7171289b64f4c9b448a70c995be
xaluoqone <1617321643@qq.com>
2021-03-27 17:03:27 +0800

    update project structure

--- CustomView/app/src/main/java/com/xaluoqone/customview/MainActivity.kt
+++ CustomView/app/src/main/java/com/xaluoqone/customview/MainActivity.kt
@@ -1,37 +1,11 @@
 package com.xaluoqone.customview
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import com.xaluoqone.customview.widget.DashboardView
-import com.xaluoqone.customview.widget.PieView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var dashboardView: DashboardView
-    private lateinit var speedPlus: Button
-    private lateinit var pieView: PieView
-    private lateinit var pieSwitch: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        dashboardView = findViewById(R.id.dashboard)
-        speedPlus = findViewById(R.id.speedPlus)
-        pieView = findViewById(R.id.pieView)
-        pieSwitch = findViewById(R.id.pieSwitch)
-
-        pieView.setOnClickListener {
-            startActivity(Intent(this@MainActivity, MainActivity2::class.java))
-        }
-
-        speedPlus.setOnClickListener {
-            dashboardView.speed++
-        }
-
-        pieSwitch.setOnClickListener {
-            pieView.currentPieIndex++
-        }
     }
 }
---
+++ CustomView/app/src/main/java/com/xaluoqone/customview/MainActivity1.kt
@@ -0,0 +1,37 @@
+package com.xaluoqone.customview
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.xaluoqone.customview.widget.DashboardView
+import com.xaluoqone.customview.widget.PieView
+
+class MainActivity1 : AppCompatActivity() {
+    private lateinit var dashboardView: DashboardView
+    private lateinit var speedPlus: Button
+    private lateinit var pieView: PieView
+    private lateinit var pieSwitch: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main1)
+
+        dashboardView = findViewById(R.id.dashboard)
+        speedPlus = findViewById(R.id.speedPlus)
+        pieView = findViewById(R.id.pieView)
+        pieSwitch = findViewById(R.id.pieSwitch)
+
+        pieView.setOnClickListener {
+            startActivity(Intent(this@MainActivity1, MainActivity2::class.java))
+        }
+
+        speedPlus.setOnClickListener {
+            dashboardView.speed++
+        }
+
+        pieSwitch.setOnClickListener {
+            pieView.currentPieIndex++
+        }
+    }
+}
