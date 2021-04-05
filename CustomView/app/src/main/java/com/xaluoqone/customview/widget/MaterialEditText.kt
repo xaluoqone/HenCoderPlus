ede3c53f39f8a707233ea912c17fd17ebff23def
xaluoqone <1617321643@qq.com>
2021-04-05 20:19:08 +0800

    update MaterialEditText

--- CustomView/app/src/main/java/com/xaluoqone/customview/widget/MaterialEditText.kt
+++ CustomView/app/src/main/java/com/xaluoqone/customview/widget/MaterialEditText.kt
@@ -13,10 +13,10 @@ import com.xaluoqone.customview.R
 import com.xaluoqone.customview.ex.dp
 import com.xaluoqone.customview.ex.sp
 
-class MaterialEditText(context: Context, attrs: AttributeSet? = null) :
-    AppCompatEditText(context, attrs) {
+class MaterialEditText(context : Context , attrs : AttributeSet? = null) : AppCompatEditText(context , attrs) {
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val topHintX = 4.dp
+    private var focused = false
     var topHintY = 43.dp
         set(value) {
             field = value
@@ -38,26 +38,22 @@ class MaterialEditText(context: Context, attrs: AttributeSet? = null) :
     private var topHintShowing = false
 
     private val topHintTextSizeChangeAnimator by lazy {
-        ObjectAnimator.ofFloat(this, "topHintTextSize", textSize, 12.sp)
+        ObjectAnimator.ofFloat(this , "topHintTextSize" , textSize , 12.sp)
     }
 
     private val topHintTextColorChangeAnimator by lazy {
-        ObjectAnimator.ofArgb(this, "topHintTextColor", hintTextColors.defaultColor, Color.BLACK)
+        ObjectAnimator.ofArgb(this , "topHintTextColor" , hintTextColors.defaultColor , Color.BLACK)
     }
 
     private val topHintYChangeAnimator by lazy {
-        ObjectAnimator.ofFloat(this, "topHintY", 43.dp, 18.dp)
+        ObjectAnimator.ofFloat(this , "topHintY" , 43.dp , 18.dp)
     }
 
     private val topHintAnimatorSet = AnimatorSet().apply {
-        playTogether(
-            topHintTextSizeChangeAnimator,
-            topHintYChangeAnimator,
-            topHintTextColorChangeAnimator
-        )
-        duration = 300
+        playTogether(topHintTextSizeChangeAnimator , topHintYChangeAnimator , topHintTextColorChangeAnimator)
+        duration = 100
     }
-    private var mShowTopHint: Boolean
+    private var mShowTopHint : Boolean
 
     private val hintText = hint.toString()
     var showTopHint = false
@@ -65,47 +61,57 @@ class MaterialEditText(context: Context, attrs: AttributeSet? = null) :
             field = value
             if (field) {
                 hint = ""
-                setPadding(paddingLeft, paddingTop + 18.dp.toInt(), paddingRight, paddingBottom)
-            } else {
+                setPadding(paddingLeft , paddingTop + 18.dp.toInt() , paddingRight , paddingBottom)
+                topHintShowing = if (!focused && text.isNullOrEmpty()) {
+                    topHintY = 43.dp
+                    topHintTextSize = textSize
+                    topHintTextColor = hintTextColors.defaultColor
+                    false
+                }
+                else {
+                    topHintY = 18.dp
+                    topHintTextSize = 12.sp
+                    topHintTextColor = Color.BLACK
+                    true
+                }
+            }
+            else {
                 hint = hintText
-                setPadding(paddingLeft, paddingTop - 18.dp.toInt(), paddingRight, paddingBottom)
+                setPadding(paddingLeft , paddingTop - 18.dp.toInt() , paddingRight , paddingBottom)
             }
             invalidate()
         }
 
     init {
-        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.MaterialEditText)
-        mShowTopHint = typedArray.getBoolean(R.styleable.MaterialEditText_showTopHint, true)
+        val typedArray = context.obtainStyledAttributes(attrs , R.styleable.MaterialEditText)
+        mShowTopHint = typedArray.getBoolean(R.styleable.MaterialEditText_showTopHint , true)
         if (mShowTopHint) {
             showTopHint = mShowTopHint
         }
         typedArray.recycle()
     }
 
-    override fun onFocusChanged(focused: Boolean, direction: Int, previouslyFocusedRect: Rect?) {
-        super.onFocusChanged(focused, direction, previouslyFocusedRect)
+    override fun onFocusChanged(focused : Boolean , direction : Int , previouslyFocusedRect : Rect?) {
+        super.onFocusChanged(focused , direction , previouslyFocusedRect)
+        this.focused = focused
         if (text.isNullOrEmpty() && showTopHint) {
             if (focused && !topHintShowing) {
                 topHintAnimatorSet.start()
                 topHintShowing = true
-            } else if (topHintShowing) {
+            }
+            else if (topHintShowing) {
                 topHintAnimatorSet.reverse()
                 topHintShowing = false
             }
         }
-        if (focused && !showTopHint) {
-            topHintY = 18.dp
-            topHintTextSize = 12.sp
-            topHintTextColor = Color.BLACK
-        }
     }
 
-    override fun onDraw(canvas: Canvas) {
+    override fun onDraw(canvas : Canvas) {
         super.onDraw(canvas)
         if (showTopHint) {
             paint.textSize = topHintTextSize
             paint.color = topHintTextColor
-            canvas.drawText(hintText, topHintX, topHintY, paint)
+            canvas.drawText(hintText , topHintX , topHintY , paint)
         }
     }
 }
